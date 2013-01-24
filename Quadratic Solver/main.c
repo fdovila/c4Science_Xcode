28e9c286d46d5fa9aee2d6e331519b25ce192720
Fdo AvilaR <favilar@gmail.com>
2013-01-24 16:27:12 +0000

    Initial Commit

---
+++ Quadratic Solver/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Quadratic Solver
+//
+//  Created by Wuachi Mingo on 1/24/13.
+//  Copyright (c) 2013 40 hertz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
