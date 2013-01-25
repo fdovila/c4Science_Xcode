e385945d07683303a88c8db9cdc7a9a2f86f51ae
Fdo AvilaR <favilar@gmail.com>
2013-01-25 18:03:14 +0000

    trying to deal with the complex numbers in case 1

--- Quadratic Solver/main.c
+++ Quadratic Solver/main.c
@@ -3,22 +3,25 @@
 //  Quadratic Solver
 //
 #include <stdio.h>
+#include <complex.h>
 
-int quad_roots(double * A, double * roots);
+int quad_roots(double * A, double * r_roots); //declaring that there's a function quad_roots
 
 int main(void)
 {
     /* Create "double" type arrays to store coeffients A (3) and the roots (2) */
-    double A[3], roots[2];
+    double A[3], r_roots[2];
+    double _Complex c_roots[2];
     
     /* Prompt for the coefficients */
     printf("Enter coefficients a, b, and c, of the Quadratic Equation in the form:\na*(x)^2 + bx + c\nin the order a, b, then c (separated by spaces):");
     // for the sake of clarity, a, b, c will be referred as A[2], A[1], A[0]
     scanf("%lf %lf %lf", A+2, A+1, A);
     
+    
     /* Solve equation using function "quad_roots" (on file "quadSol.c")" */
     /* depending on the kind of discriminants */
-    switch (quad_roots(A, roots)) {
+    switch (quad_roots(A, r_roots)) {
         
         /* A[2] == A[1] == 0, where A[0] != 0, therefore not an equation */
         case -3:
@@ -32,22 +35,23 @@ int main(void)
             
         /* A[0] == 0 and A[1] != 0, this is a LINEAR equation, with just ONE solution */
         case -1:
-            printf("case -1\nx = %f\nHey Einstein! This is not quadratic, it's a LINEAR equation\n", roots[0]);
+            printf("case -1\nx = %f\nHey Einstein! This is not quadratic, it's a LINEAR equation\n",  r_roots[0]);
             break;
             
         /* (A[1])^2 > 4(A[2]*A[0]), discriminant > 0, there are two different real roots */
         case 0:
-            printf("case 0\nx = %f\nx = %f\n", roots[0], roots[1]);
+            printf("case 0\nx = %10.20f\nx = %10.20f\n",  r_roots[0],  r_roots[1]);
             break;
             
         /* (A[1])^2 < 4(A[2]*A[0]), discriminant < 0, solutions are complex conjugate roots */
+        /* not sure here...!!!! */
         case 1:
-            printf("case 1\nx = %f (is a real number)\nx = %f(is an imaginary number)\n", roots[0], roots[1]);
+            printf("case 1\nx = %10.20f (is a real number)\nx = i%10.20f (is an imaginary number)\n", creal(c_roots[1]), cimag(c_roots[1]));
             break;
             
         /* (A[1])^2 == 4(A[2]*A[0]), discriminant == 0, roots[0] == roots[1] ? */
         case 2:
-            printf("case 2\nx = %f is equal to:\nx = %f\n", roots[0], roots[1]);
+            printf("case 2\nx = %10.20f is equal to:\nx = %10.20f\n",  r_roots[0],  r_roots[1]);
             break;
 
     }
--- Quadratic Solver/quadSol.c
+++ Quadratic Solver/quadSol.c
@@ -1,7 +1,9 @@
 #include <math.h>
-int quad_roots(double * A, double * roots)
+#include <complex.h>
+
+int quad_roots(double * A, double * r_roots, double _Complex * c_roots)
 {
-    // int discriminant = (A[1]*A[1]) - (4*(A[2]*A[0]));
+    // int discriminant = (A[1]*A[1]) - (4*(A[2]*A[0])), not sure if it could be useful touhg;
     if (A[2] == 0)
     {
         if (A[1] == 0)
@@ -19,7 +21,7 @@ int quad_roots(double * A, double * roots)
         }else
         {
             //it's a linear equation with one solution in roots[0]
-            roots[0] = -(A[0]/A[1]);
+            r_roots[0] = -(A[0]/A[1]);
             return -1;
         }
     }
@@ -29,10 +31,10 @@ int quad_roots(double * A, double * roots)
         {
             //two different real roots
             // calculate x with (+)root and store it in roots[0]
-            roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+            r_roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
             
             // calculate x with (-)root and store it in roots[1]
-            roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+            r_roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
             return 0;
         }
         else
@@ -40,16 +42,16 @@ int quad_roots(double * A, double * roots)
             if ((A[1]*A[1]) < (4*(A[2]*A[0])))
             {
                 // solutions are complex conjugate roots: roots[0] is real, roots[1] is i
-                /* not sure here... */
-                roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
-                roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+                /* not sure here...!!!! */
+                c_roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+                c_roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
                 return 1;
             }
             else
             {
                 // so this means (A[1]*A[1]) == (4*(A[2]*A[0])), so the roots[0] and [1] are equal
-                roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
-                roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+                r_roots[0] = ((-(A[1])) + (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
+                r_roots[1] = ((-(A[1])) - (sqrt((A[1]*A[1]) - (4*(A[2]*A[0]))))) / (2*(A[2]));
                 return 2;
             }
         }
